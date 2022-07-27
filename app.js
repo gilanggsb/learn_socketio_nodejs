3c28df6bcf33c4b13c5d313f61afc6e8b5f32146
gilanggsb <gilanggsb@gmail.com>
2022-07-27 14:15:42 +0700

    fix bug on callFunction and etc

--- app.js
+++ app.js
@@ -21,16 +21,16 @@ io.on("connection", (socket) => {
 
   socket.on("chatMessage", (param) => {
     let message;
-    historyData.data.chatData.map((chat) => {
-      if (chat.chat_type == param.chat_type && chat.user_id == param.user_id) {
-        message = chat;
+    for (var chat of historyData.data.chatData) {
+      if (chat.user_id == param.user_id) {
+        message = cloneObject(chat);
         message.user_role = param.user_role;
         message.data.message = param.message;
         message.infoAssign = param.infoAssign;
-        return;
+        message.chat_type = param.chat_type;
+        break;
       }
-    });
-
+    }
     historyData.data.chatData.push(message);
     io.emit("message", message);
   });
@@ -43,20 +43,32 @@ io.on("connection", (socket) => {
   socket.on("callFunction", (param) => {
     let murid, guru;
     let result = [];
-    for (var chat of historyData.data.chatData) {
-      if (chat.user_id == param.infoAssign.id_user_student) {
-        murid = chat;
-        murid.counter = param.counter;
+    for (const chat of historyData.data.chatData) {
+      if (
+        chat.user_id == param.infoAssign.id_user_student &&
+        murid == undefined
+      ) {
+        murid = cloneObject(chat);
+        murid.action.counter = param.counter;
         murid.data.message = param.message;
+        murid.chat_type = 4;
         murid.infoAssign = param.infoAssign;
       }
-      if (chat.user_id == param.infoAssign.id_user_teacher) {
-        guru = chat;
-        guru.counter = param.counter;
+      if (
+        chat.user_id == param.infoAssign.id_user_teacher &&
+        guru == undefined
+      ) {
+        guru = cloneObject(chat);
+        guru.action.counter = param.counter;
         guru.data.message = param.message;
+        guru.chat_type = 2;
         guru.infoAssign = param.infoAssign;
       }
-      if (murid != null && guru != null) break;
+      if (
+        (murid != null && guru != null) ||
+        (murid != undefined && guru != undefined)
+      )
+        break;
     }
     historyData.data.chatData.push(murid, guru);
     result.push(murid, guru);
@@ -65,17 +77,15 @@ io.on("connection", (socket) => {
 
   socket.on("sendFile", (param) => {
     let resultFile;
-    historyData.data.chatData.map((chat) => {
-      if (
-        chat.chat_type === param.chat_type &&
-        chat.user_id === param.user_id
-      ) {
-        resultFile = chat;
+    for (var chat of historyData.data.chatData) {
+      if (chat.user_id === param.user_id) {
+        resultFile = cloneObject(chat);
         resultFile.data.url = param.url;
+        resultFile.chat_type = param.chat_type;
         resultFile.data.fileName = param.fileName;
-        return;
+        break;
       }
-    });
+    }
     historyData.data.chatData.push(resultFile);
     io.emit("notif", resultFile);
   });
@@ -88,6 +98,11 @@ io.on("connection", (socket) => {
     io.emit("countUserOnline", countUserOnline);
   });
 });
+
+function cloneObject(obj) {
+  let temp = JSON.stringify(obj);
+  return JSON.parse(temp);
+}
 
 server.listen(port, () => {
   console.log("App is running on port " + port);
